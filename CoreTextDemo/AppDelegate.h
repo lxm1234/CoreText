71e851177358938248c1d2e5d38f5e28d360ccb4
lxm <1027381389@qq.com>
2018-01-05 10:26:45 +0800

    Initial Commit

---
+++ CoreTextDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreTextDemo
+//
+//  Created by lxm on 2018/1/5.
+//  Copyright © 2018年 lixiaomeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreTextDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CoreTextDemo
+//
+//  Created by lxm on 2018/1/5.
+//  Copyright © 2018年 lixiaomeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
